1c53fc82a29450b3375973bb663c02e671ce1916
Gustavo Modena <gustavomodena@Gustavos-MacBook-Air.local>
2023-12-03 14:46:57 -0300

    chore: create recipes, recipes categories and fruits categories, including creating some of them

---
+++ src/api/fruits-category/controllers/fruits-category.ts
@@ -0,0 +1,7 @@
+/**
+ * fruits-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::fruits-category.fruits-category');
---
+++ src/api/fruits-category/routes/fruits-category.ts
@@ -0,0 +1,7 @@
+/**
+ * fruits-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::fruits-category.fruits-category');
